ba30b36e191cb429179a70e5ac72a1429ef0ba94
jmarcooo <marco.odono@gmail.com>
2026-03-14 03:39:58 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/rpg-dungeon-game/', // This is required for GitHub Pages
+})
